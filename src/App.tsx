e687d6ccaea7d76b1b3174283c83e01b069079b6
mxcul1 <mxcul1@gmail.com>
2021-08-17 18:01:13 +1000

    fixed note object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import logo from "./logo.svg";
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { GraphQLResult } from "@aws-amplify/api-graphql";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import { API } from "aws-amplify";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
+import React, { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { GraphQLResult } from "@aws-amplify/api-graphql";
+import { listNotes } from "./graphql/queries";
+import logo from "./logo.svg";
+
+type Note = {
+  id: string;
+  name: string;
+  description: string;
+};
 
 const initialFormState = { name: "", description: "" };
+
 type ApiResponse = {
   listNotes: {
-    items: any;
+    items: Array<Note>;
     nextToken: any;
   };
 };
+
 function App() {
-  const [notes, setNotes] = useState([] as any);
+  const [notes, setNotes] = useState<Array<Note>>([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
@@ -29,7 +38,7 @@ function App() {
     const apiData = (await API.graphql({
       query: listNotes,
     })) as GraphQLResult<ApiResponse>;
-    setNotes(apiData?.data?.listNotes);
+    setNotes(apiData?.data?.listNotes?.items || []);
   }
 
   async function createNote() {
@@ -38,12 +47,13 @@ function App() {
       query: createNoteMutation,
       variables: { input: formData },
     });
-    setNotes([...notes, formData]);
+    const newNote: Note = { ...formData, id: uuid() };
+    setNotes([...notes, newNote]);
     setFormData(initialFormState);
   }
 
-  async function deleteNote({ id }: { id: number }) {
-    const newNotesArray = notes.filter((note: any) => note.id !== id);
+  async function deleteNote(id: string) {
+    const newNotesArray = notes.filter((note) => note.id !== id);
 
     setNotes(newNotesArray);
     await API.graphql({
@@ -68,31 +78,31 @@ function App() {
         >
           {" Click here for resumé"}
         </a>
-        <div className = "Notes">
-        {/* <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <button onClick={createNote}>Create Note</button> */}
-        {/* <div style={{marginBottom: 30}}>
-        {
-          notes.map((note:any) => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-            </div>
-          ))
-        }
-      </div> */}
-      </div>
+        <div className="Notes">
+          <h1>My Notes App</h1>
+          <input
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            placeholder="Note name"
+            value={formData.name}
+          />
+          <input
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+            placeholder="Note description"
+            value={formData.description}
+          />
+          <button onClick={createNote}>Create Note</button>
+          <div style={{ marginBottom: 30 }}>
+            {notes.map((note) => (
+              <div key={note.id}>
+                <h2>{note.name}</h2>
+                <p>{note.description}</p>
+                <button onClick={() => deleteNote(note.id)}>Delete note</button>
+              </div>
+            ))}
+          </div>
+        </div>
       </header>
     </div>
   );
